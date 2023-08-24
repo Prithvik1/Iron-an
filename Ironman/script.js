38c725976a12044237af8082bd99dcbc65b0d109
Prithvik1 <141633521+Prithvik1@users.noreply.github.com>
2023-08-24 09:32:01 +0530

    Add files via upload

---
+++ Ironman/script.js
@@ -0,0 +1,28 @@
+// <!-- Ironman - background, helmet, eyes, face-plate  -->
+const imagebackground = document.getElementById("background")
+const buttonbackground = document.getElementById("btn-background")
+
+buttonbackground.oninput = () =>{
+  imagebackground.style.fill = buttonbackground.value;
+}
+
+const imagehelmet = document.getElementById("helmet")
+const buttonhelmet = document.getElementById("btn-helmet")
+
+buttonhelmet.oninput = () =>{
+  imagehelmet.style.fill = buttonhelmet.value;
+}
+
+const imageeyes = document.getElementById("eyes")
+const buttoneyes = document.getElementById("btn-eyes")
+
+buttoneyes.oninput = () =>{
+  imageeyes.style.fill = buttoneyes.value;
+}
+
+const imagefaceplate = document.getElementById("face-plate")
+const buttonfaceplate = document.getElementById("btn-face-plate")
+
+buttonfaceplate.oninput = () =>{
+  imagefaceplate.style.fill = buttonfaceplate.value;
+}
